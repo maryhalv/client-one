5844bb5686b8c164689795bd0b015cae964aa847
Mary Halvorsen <mhalvorsen98@gmail.com>
2020-08-01 23:20:54 -0400

    resume one

--- src/view/components/Resume.js
+++ src/view/components/Resume.js
@@ -1,12 +1,15 @@
 import React, {useState, useLayoutEffect} from 'react';
 import './styles/resume.css';
+import resume from './documents/resume_prof.pdf';
+
 
 export function Resume() {
-
    return (
      <React.Fragment>
      <div id="main-resume">
-     Resume
+     <object id="resume-view" data={resume} type="application/pdf">
+       <p> <a href={resume}></a></p>
+     </object>
      </div>
      </React.Fragment>
    );
