756ee1f5d17eb96f18dc095769ec72b3ac088309
Mary Halvorsen <mhalvorsen98@gmail.com>
2020-08-05 23:59:16 -0400

    headshot slideshow

--- src/view/components/About.js
+++ src/view/components/About.js
@@ -1,19 +1,47 @@
 import React, {useState, useLayoutEffect, useEffect} from 'react';
 import './styles/about.css';
 import headshot_1 from '../images/headshot_1.jpeg';
+import headshot_2 from '../images/headshot_2.jpeg';
+import headshot_3 from '../images/headshot_3.JPG';
+import headshot_4 from '../images/headshot_4.JPG';
 
 export function About(props) {
 
+const[headView, setHead] = useState(headshot_1);
+const[nextView, setNext]= useState(1);
+
 useEffect(() => {
     window.scrollTo(0,0);
-}, []);
+});
+
+useEffect(() => {
+ setTimeout(() => {
+    switch(nextView){
+    case 1:
+    setHead(headshot_2);
+    setNext(2);
+    break;
+    case 2:
+    setHead(headshot_3);
+    setNext(3);
+    break;
+    case 3:
+    setHead(headshot_4);
+    setNext(4);
+    break;
+    default:
+    setHead(headshot_1);
+    setNext(1);
+    }
+ } ,3000)
+});
 
 return(
     <React.Fragment>
         <div id="about-container">
         <div id="main-about">
         <div id="about-headshot">
-           <img id="img-headshot-about"src={headshot_1} />
+           <img id="img-headshot-about"src={headView} />
         </div>
         <div id="welcome-header">
             Welcome!
